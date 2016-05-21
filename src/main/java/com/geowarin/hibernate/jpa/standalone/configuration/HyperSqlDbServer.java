16df1b823124b5e18b9c36b28bd9e248548955df
Elvis de Freitas <edigitalb@gmail.com>
2016-05-20 22:57:53 -0300

    creating HSQLDB factory

---
+++ src/main/java/com/geowarin/hibernate/jpa/standalone/configuration/HyperSqlDbServer.java
@@ -0,0 +1,83 @@
+package com.geowarin.hibernate.jpa.standalone.configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.hsqldb.Server;
+import org.hsqldb.persist.HsqlProperties;
+import org.hsqldb.server.ServerAcl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.SmartLifecycle;
+
+public class HyperSqlDbServer implements SmartLifecycle {
+    private final Logger logger = LoggerFactory.getLogger(HyperSqlDbServer.class);
+    private HsqlProperties properties;
+    private Server server;
+    private boolean running = false;
+
+    public HyperSqlDbServer() {
+
+        Properties properties = new Properties();
+        properties.setProperty("server.port", "63535");
+        properties.setProperty("server.database.0", "file:d:/hsqldb/testdb");
+        properties.setProperty("server.dbname.0", "testdb");
+        properties.setProperty("server.remote_open", "true");
+        properties.setProperty("server.silent", "true");
+        this.properties = new HsqlProperties(properties);
+
+    }
+    public HyperSqlDbServer(Properties props) {
+        properties = new HsqlProperties(props);
+    }
+
+    @Override
+    public boolean isRunning() {
+        if (server != null)
+            server.checkRunning(running);
+        return running;
+    }
+
+    @Override
+    public void start() {
+        if (server == null) {
+            logger.info("Starting HSQL server...");
+            server = new Server();
+            try {
+                server.setProperties(properties);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ServerAcl.AclFormatException e) {
+                e.printStackTrace();
+            }
+            server.start();
+            System.out.println(server.getAddress());
+            running = true;
+        }
+    }
+
+    @Override
+    public void stop() {
+        logger.info("Stopping HSQL server...");
+        if (server != null) {
+            server.stop();
+            running = false;
+        }
+    }
+
+    @Override
+    public int getPhase() {
+        return 0;
+    }
+
+    @Override
+    public boolean isAutoStartup() {
+        return true;
+    }
+
+    @Override
+    public void stop(Runnable runnable) {
+        stop();
+        runnable.run();
+    }
+}
